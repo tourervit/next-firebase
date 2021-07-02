6fefe96a595045d3d67e98b3543fa9160b8ff9c3
Vitaliy Ponomarev <tourervit@gmail.com>
2021-07-02 11:12:34 +0300

    Updated deps

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,4 +1,3 @@
 /// <reference types="next" />
-/// <reference types="next/router" />
 /// <reference types="next/types/global" />
-/// <reference types="react-firebase-hooks/auth" />
+/// <reference types="next/image-types/global" />
